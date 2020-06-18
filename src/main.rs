89ac5b4232243690939963525df8f9d4067f8385
Jonathan Nilsson <jonathan@voysys.se>
2020-06-19 00:53:40 +0200

    Step 3

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,37 @@ fn get_payload(input: &str) -> Result<&str, Box<dyn Error>> {
         .ok_or("Error")?)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let step_1 = fs::read_to_string("step_1.txt")?;
+fn step_1() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("step_1.txt")?;
+    let payload = get_payload(&input)?;
 
-    let payload_1 = get_payload(&step_1)?;
-
-    let decoded = ascii85::decode(payload_1)?;
+    let decoded = ascii85::decode(payload)?;
 
     fs::write("step_2.txt", String::from_utf8_lossy(&decoded).as_bytes())?;
+
+    Ok(())
+}
+
+fn step_2() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("step_2.txt")?;
+
+    let payload = get_payload(&input)?;
+
+    let mut decoded = ascii85::decode(payload)?;
+
+    for byte in &mut decoded {
+        *byte = (*byte ^ 0b01010101).rotate_right(1)
+    }
+
+    fs::write("step_3.txt", String::from_utf8_lossy(&decoded).as_bytes())?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    step_1()?;
+    step_2()?;
 
     Ok(())
 }
