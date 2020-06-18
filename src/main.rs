25e6d058b7020245d4ad1b8669cb4b9cce2b4842
Jonathan Nilsson <jonathan@voysys.se>
2020-06-19 01:39:57 +0200

    Step 4 done

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,55 @@ fn step_2() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn step_3() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("step_3.txt")?;
+
+    let payload = get_payload(&input)?;
+
+    let decoded = ascii85::decode(payload)?;
+
+    let decoded = decoded.into_iter().filter(|byte| {
+        let data = *byte & 0b1111_1110;
+        let parity = *byte & 0b1;
+        data.count_ones() & 0b1 == parity as u32
+    }).collect::<Vec<_>>();
+
+    let mut result = Vec::new();
+
+    for chunk in decoded.chunks_exact(8) {
+        result.push(((chunk[0] & 0b1111_1110) << 0) | ((chunk[1] & 0b1000_0000) >> 7));
+        result.push(((chunk[1] & 0b0111_1110) << 1) | ((chunk[2] & 0b1100_0000) >> 6));
+        result.push(((chunk[2] & 0b0011_1110) << 2) | ((chunk[3] & 0b1110_0000) >> 5));
+        result.push(((chunk[3] & 0b0001_1110) << 3) | ((chunk[4] & 0b1111_0000) >> 4));
+        result.push(((chunk[4] & 0b0000_1110) << 4) | ((chunk[5] & 0b1111_1000) >> 3));
+        result.push(((chunk[5] & 0b0000_0110) << 5) | ((chunk[6] & 0b1111_1100) >> 2));
+        result.push(((chunk[6] & 0b0000_0010) << 6) | ((chunk[7] & 0b1111_1110) >> 1));
+    }
+
+    fs::write("step_4.txt", String::from_utf8_lossy(&result).as_bytes())?;
+
+    Ok(())
+}
+
+fn step_4() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("step_4.txt")?;
+
+    let payload = get_payload(&input)?;
+
+    let decoded = ascii85::decode(payload)?;
+
+    fs::write("step_5.txt", String::from_utf8_lossy(&decoded).as_bytes())?;
+
+    Ok(())
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
 
     step_1()?;
     step_2()?;
+    step_3()?;
+    step_4()?;
 
     Ok(())
 }
