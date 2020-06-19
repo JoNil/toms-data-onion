589fbebcadaf81ff7facad27b51ef9509f7d1565
Jonathan Nilsson <jonathan@voysys.se>
2020-06-19 02:00:46 +0200

    Step 5 done

--- src/main.rs
+++ src/main.rs
@@ -41,11 +41,14 @@ fn step_3() -> Result<(), Box<dyn Error>> {
 
     let decoded = ascii85::decode(payload)?;
 
-    let decoded = decoded.into_iter().filter(|byte| {
-        let data = *byte & 0b1111_1110;
-        let parity = *byte & 0b1;
-        data.count_ones() & 0b1 == parity as u32
-    }).collect::<Vec<_>>();
+    let decoded = decoded
+        .into_iter()
+        .filter(|byte| {
+            let data = *byte & 0b1111_1110;
+            let parity = *byte & 0b1;
+            data.count_ones() & 0b1 == parity as u32
+        })
+        .collect::<Vec<_>>();
 
     let mut result = Vec::new();
 
@@ -71,14 +74,53 @@ fn step_4() -> Result<(), Box<dyn Error>> {
 
     let decoded = ascii85::decode(payload)?;
 
-    fs::write("step_5.txt", String::from_utf8_lossy(&decoded).as_bytes())?;
+    let key: [u8; 32] = [
+        decoded[0] ^ b'=',
+        decoded[1] ^ b'=',
+        decoded[2] ^ b'[',
+        decoded[3] ^ b' ',
+        decoded[4] ^ b'L',
+        decoded[5] ^ b'a',
+        decoded[6] ^ b'y',
+        decoded[7] ^ b'e',
+        decoded[8] ^ b'r',
+        decoded[9] ^ b' ',
+        decoded[10] ^ b'4',
+        decoded[11] ^ b'/',
+        decoded[12] ^ b'5',
+        decoded[45] ^ b'=',
+        decoded[46] ^ b'=',
+        decoded[47] ^ b'=',
+        decoded[48] ^ b'=',
+        decoded[49] ^ b'=',
+        decoded[50] ^ b'=',
+        decoded[51] ^ b'=',
+        decoded[52] ^ b'=',
+        decoded[53] ^ b'=',
+        decoded[54] ^ b'=',
+        decoded[55] ^ b'=',
+        decoded[56] ^ b'=',
+        decoded[57] ^ b'=',
+        decoded[58] ^ b'=',
+        decoded[59] ^ b'=',
+        decoded[60] ^ b'\n',
+        decoded[29] ^ b'c',
+        decoded[30] ^ b' ',
+        decoded[31] ^ b']',
+    ];
+
+    let decrypted = decoded
+        .iter()
+        .zip(key.iter().cycle())
+        .map(|(ciphertext, key)| *ciphertext ^ *key)
+        .collect::<Vec<_>>();
+
+    fs::write("step_5.txt", String::from_utf8_lossy(&decrypted).as_bytes())?;
 
     Ok(())
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
-
     step_1()?;
     step_2()?;
     step_3()?;
